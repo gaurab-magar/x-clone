c0714eec8f86c2c3f93c3f2341d63eb6c3b85cd9
Your Name <gdikshan10@gmail.com>
2024-06-13 02:59:58 +0545

    env file added for both server and client side

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: process.env.API_URL
+        API_URL: process.env.API_URL,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
     },
     images: {
         domains:['image.cnbcfm.com','a57.foxnews.com','cdn.vox-cdn.com','cdn.cnn.com','lh3.googleusercontent.com'],
