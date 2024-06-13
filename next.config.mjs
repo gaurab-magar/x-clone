9b909e0307a5a9033dfba8021f163939fdcaabcd
Your Name <gdikshan10@gmail.com>
2024-06-13 11:51:48 +0545

    updated next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     env: {
         API_URL: process.env.API_URL,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
     },
     images: {
         domains:['image.cnbcfm.com','a57.foxnews.com','cdn.vox-cdn.com','cdn.cnn.com','lh3.googleusercontent.com'],
