a185a24dcee7860fa24ff013c650d0527c4ee73a
Your Name <gdikshan10@gmail.com>
2024-06-14 12:18:33 +0545

    file successfully added to the firestore for the feed

--- src/app/Components/Input.js
+++ src/app/Components/Input.js
@@ -5,14 +5,17 @@ import React, { useEffect, useRef, useState } from 'react'
 import { CiCirclePlus } from "react-icons/ci";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '@/Firebase';
-
+import { addDoc,collection,getFirestore,serverTimestamp } from 'firebase/firestore';
 const Input = () => {
     const {data:session} = useSession();
     const imagePicRef = useRef();
 
-    const [imageFileUrl , setImageFileUrl] = useState();
+    const [imageFileUrl , setImageFileUrl] = useState(null);
     const [selectedImage , setSelectedImage] = useState(null);
     const [loading , setLoading] = useState(false);
+
+    const [text , setText] = useState('');
+    const db = getFirestore(app)
 
     function addImageToPost (e){
         const file = e.target.files[0];
@@ -52,13 +55,29 @@ const Input = () => {
             }
         )
     }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const docRef = await addDoc(collection(db,'posts'),{
+            uid: session.user.uid,
+            name: session.user.name,
+            username: session.user.username,
+            text,
+            ProfileImg: session.user.image,
+            image: imageFileUrl,
+            Timestamp: serverTimestamp()
+        });
+        setText('');
+        setImageFileUrl(null);
+        setSelectedImage(null);
+    }
   return (
     <div className='py-2'>
         {session && (
             <div>
                 <div className='border-b border-gray-300 flex items-start justify-center gap-4 w-full'>
                     <Image src={session.user.image} alt='user-img' width={40} height={40} className='rounded-full' />
-                    <textarea rows={2} type='text' placeholder='Whats happening ?' className='p-1 outline-none w-full' />
+                    <textarea value={text} onChange={(e) => setText(e.target.value)} rows={2} type='text' placeholder='Whats happening ?' className='p-1 outline-none w-full' />
                 </div>
                     {
                         selectedImage && (
@@ -69,8 +88,8 @@ const Input = () => {
                     }
                 <div className='flex items-center justify-between p-2'>
                     <CiCirclePlus onClick={()=> imagePicRef.current.click()} className='text-4xl text-blue-600 hover:text-blue-800 hover:scale-105 transition duration-200 cursor-pointer' />
-                    <input hidden type='file' ref={imagePicRef} accept='/image/*' onChange={addImageToPost} />
-                    <button className='bg-blue-400 text-white font-semibold rounded-full px-4 py-1 hover:scale-105 transition duration-200'>Post</button>
+                    <input hidden type='file' ref={imagePicRef} accept='image/*' onChange={addImageToPost} />
+                    <button disabled={text.trim() === '' || loading} onClick={handleSubmit} className='disbaled:bg-blue-200 bg-blue-400 text-white font-semibold rounded-full px-4 py-1 hover:scale-105 transition duration-200'>Post</button>
                 </div>
             </div>
         )}
